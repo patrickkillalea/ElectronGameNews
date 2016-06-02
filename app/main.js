934ae2aa36a12537262add5f2cb6be0cfa9673e9
patrickkillalea <killalea.patrick@gmail.com>
2016-06-02 09:29:52 +0100

    Removing files to fix Mac OS issue

--- app/main.js
+++ app/main.js
@@ -22,6 +22,9 @@ app.on('ready', function() {
     // Create the browser window.
     mainWindow = new BrowserWindow({ width: 1200, height: 800, 'min-height': 600, 'min-width': 800 });
 
+    // For No Top frame
+    // , frame: false
+
     // and load the index.html of the app.
     mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
