9eb7b07f69e42f052a200e5d5c068b831c9bfe61
patrickkillalea <killalea.patrick@gmail.com>
2016-06-15 21:40:34 +0100

    TopNav tabs are now functional, added images for all games

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,28 +11,84 @@
         .config(['$routeProvider', function($routeProvider) {
             $routeProvider.when('/', {
                 templateUrl: _templateBase + '/home/home.html',
-                controller: 'homeController',
-                controllerAs: '_ctrl'
+                controller: 'homeController'
             });
-            $routeProvider.when('/wow', {
-                templateUrl: _templateBase + '/wow/wow.html',
-                controller: 'wowController',
-                controllerAs: '_ctrl',
-                activetab: 'wow'
+            $routeProvider.when('/wownews', {
+                templateUrl: _templateBase + '/wow/news.html',
+                controller: 'wowController'
             });
-            $routeProvider.when('/starcraft', {
-                templateUrl: _templateBase + '/starcraft/starcraft.html',
+            $routeProvider.when('/wowfixtures', {
+                templateUrl: _templateBase + '/wow/fixtures.html',
+                controller: 'wowController'
+            });
+            $routeProvider.when('/wowcompetitions', {
+                templateUrl: _templateBase + '/wow/competitions.html',
+                controller: 'wowController'
+            });
+            $routeProvider.when('/wowresources', {
+                templateUrl: _templateBase + '/wow/resources.html',
+                controller: 'wowController'
+            });
+            $routeProvider.when('/starcraftnews', {
+                templateUrl: _templateBase + '/starcraft/news.html',
                 controller: 'starcraftController',
                 controllerAs: '_ctrl',
                 activetab: 'starcraft'
             });
-            $routeProvider.when('/cs', {
-                templateUrl: _templateBase + '/cs/cs.html',
+            $routeProvider.when('/starcraftfixtures', {
+                templateUrl: _templateBase + '/starcraft/fixtures.html',
+                controller: 'starcraftController',
+                controllerAs: '_ctrl',
+                activetab: 'starcraft'
+            });
+            $routeProvider.when('/starcraftcompetitions', {
+                templateUrl: _templateBase + '/starcraft/competitions.html',
+                controller: 'starcraftController',
+                controllerAs: '_ctrl',
+                activetab: 'starcraft'
+            });
+            $routeProvider.when('/starcraftresources', {
+                templateUrl: _templateBase + '/starcraft/resources.html',
+                controller: 'starcraftController',
+                controllerAs: '_ctrl',
+                activetab: 'starcraft'
+            });
+            $routeProvider.when('/csnews', {
+                templateUrl: _templateBase + '/cs/news.html',
                 controller: 'csController',
                 controllerAs: '_ctrl'
             });
-            $routeProvider.when('/dota', {
-                templateUrl: _templateBase + '/dota/dota.html',
+            $routeProvider.when('/csfixtures', {
+                templateUrl: _templateBase + '/cs/fixtures.html',
+                controller: 'csController',
+                controllerAs: '_ctrl'
+            });
+            $routeProvider.when('/cscompetitions', {
+                templateUrl: _templateBase + '/cs/competitions.html',
+                controller: 'csController',
+                controllerAs: '_ctrl'
+            });
+            $routeProvider.when('/csresources', {
+                templateUrl: _templateBase + '/cs/resources.html',
+                controller: 'csController',
+                controllerAs: '_ctrl'
+            });
+            $routeProvider.when('/dotanews', {
+                templateUrl: _templateBase + '/dota/news.html',
+                controller: 'dotaController'
+            });
+            $routeProvider.when('/dotafixtures', {
+                templateUrl: _templateBase + '/dota/fixtures.html',
+                controller: 'dotaController',
+                controllerAs: '_ctrl'
+            });
+            $routeProvider.when('/dotacompetitions', {
+                templateUrl: _templateBase + '/dota/competitions.html',
+                controller: 'dotaController',
+                controllerAs: '_ctrl'
+            });
+            $routeProvider.when('/dotaresources', {
+                templateUrl: _templateBase + '/dota/resources.html',
                 controller: 'dotaController',
                 controllerAs: '_ctrl'
             });
--- app/scripts/layout/topnav.js
+++ app/scripts/layout/topnav.js
@@ -1,3 +1,32 @@
+var currGame = "";
+
+function changeGame(game) {
+    currGame = game;
+    console.log("Current game: " + currGame);
+    // console.log("Current game111: " + "#" + game + "/Fixtures.html");
+}
+
+function newsTop(currGame) {
+    console.log("News for: " + currGame);
+    window.location.href = "#" + currGame + "news";
+    console.log("Newssss for: " + "#" + currGame + "news");
+}
+
+function fixturesTop(currGame) {
+    console.log("Fixtures for: " + currGame);
+    window.location.href = "#" + currGame + "fixtures";
+}
+
+function competitionsTop(currGame) {
+    console.log("Competitions for: " + currGame);
+    window.location.href = "#" + currGame + "competitions";
+}
+
+function resourcesTop(currGame) {
+    console.log("Resources for: " + currGame);
+    window.location.href = "#" + currGame + "resources";
+}
+
 function closeApp() {
     var remote = require('remote');
     console.log("Closing Application");
